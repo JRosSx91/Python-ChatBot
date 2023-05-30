1b808459a97acb66e1115c3e7bf734b91b54457d
JRosSx91 <hazee.1491@gmail.com>
2023-05-30 16:43:27 +0200

    add message function

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import os
 import requests
 import json
+
+
+def chat_gpt3(message):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('OPENAI_KEY')}"
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": message
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()["choices"][0]["message"]["content"]
