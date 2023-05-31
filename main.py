69aba353def1b6d4e4770e2e446aa6cea98809fc
JRosSx91 <hazee.1491@gmail.com>
2023-05-31 15:32:56 +0200

    fixing issues

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 import os
-import requests
-import json
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_KEY")
 
 
-def chat_gpt3(message):
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {os.getenv('OPENAI_KEY')}"
-    }
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "user",
-                "content": message
-            }
+def chat_gpt3(user_message):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": user_message}
         ]
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    response_json = response.json()
-
-    try:
-        return response_json["choices"][0]["message"]["content"]
-    except KeyError:
-        print(f"Unexpected response: {response_json}")
-        return ""
+    )
+    return completion.choices[0].message["content"]
 
 
 def main():
